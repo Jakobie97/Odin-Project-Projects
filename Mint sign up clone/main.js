c50fe676aef0e83117c852d9d580e8427d297862
Jacob T Stockwell <JacobTStockwell@gmail.com>
2021-09-17 15:00:02 -0500

    Add files via upload

---
+++ Mint sign up clone/main.js
@@ -0,0 +1,10 @@
+let createAccountBtn = document.getElementById("createBtn");
+let EmailElement = document.getElementById("EmailEl")
+let PhoneElement = document.getElementById("PhoneEl")
+let PasswordElement = document.getElementById("PhoneEl")
+
+createAccountBtn.addEventListener('click', function() {
+
+    //TODo: Save User Input
+})
+
